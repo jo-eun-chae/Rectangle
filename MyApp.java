0153842ae0393d31f85c378d097b24d82f151eca
YuJung <youchew14@naver.com>
2020-09-14 13:23:34 +0900

    myapp-1

--- MyApp.java
+++ MyApp.java
@@ -2,32 +2,18 @@
 /**
  * Write a description of class MyApp here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2019315026 전유정)
+ * @version (2020.09.14)
  */
 public class MyApp
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class MyApp
-     */
-    public MyApp()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String[] args){
+        Rectangle r = new Rectangle(10, 20);
+        Rectangle r2 = new Rectangel(10,20);
+        
+        
+        System.out.println(r.equals(r2));
+        
+        
     }
 }
